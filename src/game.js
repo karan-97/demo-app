0712175280c90ebb6cc40436f6f1aeb775458d70
Karan Ganwani <karan.ganwani@atliq.com>
2023-10-30 15:37:08 +0530

    Feat: basic game structure completed

--- src/game.js
+++ src/game.js
@@ -2,18 +2,19 @@ import React, { useState } from "react";
 import { Board } from "./board";
 
 export default function Game() {
-  const [xIsNext, setxIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)]);
   const [currentMove, setCurrentMove] = useState(0);
-  const currentSquares = history[history.length - 1];
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
 
   function handlePlay(nextSquares) {
-    setHistory([...history, nextSquares]);
-    setxIsNext(!xIsNext);
+    const nextHistory = [...history.slice(0,currentMove+1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
   }
 
   function jumpTo(nextMove) {
-    
+    setCurrentMove(nextMove);
   }
 
   const moves = history.map((squares, move) => {
